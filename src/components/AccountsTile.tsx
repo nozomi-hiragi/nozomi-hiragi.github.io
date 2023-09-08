4a2333f4f8fe36452629e22feae2a2970f93f564
nozomi hiragi <nozomi.hiragi@gmail.com>
2023-09-08 10:26:47 +0900

    アイコンを名前から取れるように

--- src/components/AccountsTile.tsx
+++ src/components/AccountsTile.tsx
@@ -1,16 +1,5 @@
 import { ActionIcon, ActionIconProps, Flex, Group } from "@mantine/core";
-import {
-  IconAmazon,
-  IconBooth,
-  IconFanbox,
-  IconGithub,
-  IconHatenaBlog,
-  IconPatreon,
-  IconTwitch,
-  IconTwitter,
-  IconYouTube,
-  IconZenn,
-} from "@/components/Icons";
+import { getIcon } from "@/components/Icons";
 
 const AccountActionIcon = (
   props: ActionIconProps & { children: React.ReactNode },
@@ -29,73 +18,73 @@ const AccountActionIcon = (
 const accounts = [
   {
     label: "Twitter JA",
-    icon: IconTwitter,
+    icon: "twitter",
     gradient: { from: "teal", to: "blue", deg: 90 },
   },
   {
     label: "Twitter EN",
     gradient: { from: "blue", to: "violet", deg: 90 },
-    icon: IconTwitter,
+    icon: "twitter",
   },
   {
     label: "Twitter KO",
     gradient: { from: "violet", to: "pink", deg: 90 },
-    icon: IconTwitter,
+    icon: "twitter",
   },
   {
     label: "Twitch",
     gradient: { from: "rgb(90,62,133)", to: "violet", deg: 110 },
-    icon: IconTwitch,
+    icon: "twitch",
   },
   {
     label: "YouTube",
     gradient: { from: "violet.9", to: "indigo.8", deg: 100 },
-    icon: IconYouTube,
+    icon: "youtube",
   },
   {
     label: "YouTube Hobbies",
     gradient: { from: "orange.6", to: "yellow.7", deg: 110 },
-    icon: IconYouTube,
+    icon: "youtube",
   },
   {
     label: "YouTube Travel",
     gradient: { from: "green", to: "lime.6", deg: 110 },
-    icon: IconYouTube,
+    icon: "youtube",
   },
   {
     label: "GitHub",
     gradient: { from: "dark", to: "gray", deg: 90 },
-    icon: IconGithub,
+    icon: "github",
   },
   {
     label: "Hatena Blog",
     gradient: { from: "gray.0", to: "gray.2", deg: 110 },
-    icon: IconHatenaBlog,
+    icon: "hatenablog",
   },
   {
     label: "Zenn",
     gradient: { from: "rgb(62,168,255)", to: "blue", deg: 110 },
-    icon: IconZenn,
+    icon: "zenn",
   },
   {
     label: "Booth",
     gradient: { from: "red", to: "orange", deg: 110 },
-    icon: IconBooth,
+    icon: "booth",
   },
   {
     label: "Fanbox",
     gradient: { from: "rgb(35,24,22)", to: "rgb(43,51,60)", deg: 110 },
-    icon: IconFanbox,
+    icon: "fanbox",
   },
   {
     label: "Patreon",
     gradient: { from: "rgb(255,66,77)", to: "red", deg: 110 },
-    icon: IconPatreon,
+    icon: "patreon",
   },
   {
     label: "Amazon Wishlist",
     gradient: { from: "rgb(254, 156, 21)", to: "yellow", deg: 110 },
-    icon: IconAmazon,
+    icon: "amazon",
   },
 ];
 
@@ -116,7 +105,7 @@ const AccountsTile = () => {
             gradient={account.gradient}
             aria-label={account.label}
           >
-            {account.icon({ size: 40 })}
+            {getIcon(account.icon)({ size: 40 })}
           </AccountActionIcon>
         ))}
       </Group>

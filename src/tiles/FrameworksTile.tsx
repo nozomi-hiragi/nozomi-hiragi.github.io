13dc8537ed84b6850e267b8a7f0e6e51e4313d53
nozomi hiragi <nozomi.hiragi@gmail.com>
2025-04-09 20:09:44 +0900

    スタックするように変更

--- src/tiles/FrameworksTile.tsx
+++ src/tiles/FrameworksTile.tsx
@@ -1,25 +1,42 @@
-import { Card, Flex, Group, Rating, Title } from "@mantine/core";
+import { Card, Flex, Rating, SimpleGrid, Title } from "@mantine/core";
 import { Framework } from "@/models";
 
 const FrameworksTile = ({ frameworks }: { frameworks: Framework[] }) => {
   return (
     <Flex
+      w="100vw"
       h="100vh"
       bg="lime.9"
-      px={80}
-      py={50}
       align="center"
       justify="center"
     >
-      <Group gap={50} style={{ flexWrap: "nowrap" }}>
+      <SimpleGrid
+        w="100vw"
+        px={{
+          base: 20,
+          sm: 20,
+          md: 20,
+          lg: 100,
+          xl: 100,
+        }}
+        py={50}
+        cols={{
+          base: 3,
+          xs: 3,
+          sm: 4,
+          md: 6,
+          lg: 6,
+          xl: 8,
+        }}
+      >
         {frameworks.map((framework) => (
           <Card key={framework.name}>
             <Title order={5}>{framework.name}</Title>
             <Rating readOnly fractions={10} value={framework.rate} />
           </Card>
         ))}
-      </Group>
-    </Flex>
+      </SimpleGrid>
+    </Flex >
   );
 };
 
